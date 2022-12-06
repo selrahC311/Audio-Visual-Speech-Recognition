124818a48b3634786d254122bbaf0bfbb1495afd
DylanN508 <98781315+DylanN508@users.noreply.github.com>
2022-12-06 20:48:09 +0000

    Update videoReader.m

--- videoReader.m
+++ videoReader.m
@@ -1,13 +1,13 @@
 v = VideoReader('xylophone.mp4');
 vidHeight = v.Height;
-VidWidth = v.Width;
+vidWidth = v.Width;
 
 % video object, vidObj and structure, s, with fields cdata and colormap to
 % hold data. 
-s = struct('cdata', zeros(vidHeight, VidWidth, 3, 'uint8'), 'colormap', []);
+s = struct('cdata', zeros(vidHeight, vidWidth, 3, 'uint8'), 'colormap', []);
 
 % movie data structure 
-mov = struc('cdata', zeros(vidHeight, VidWidth, 3, 'uint8'), 'colormap', []);
+mov = struct('cdata', zeros(vidHeight, vidWidth, 3, 'uint8'), 'colormap', []);
 
 % movie read in as
 k = 1; 
@@ -21,10 +21,10 @@ hf = figure;
 set(hf, 'position', [150 150 vidWidth vidHeight]);
 movie(hf, mov, 1, v.FrameRate);
 
-P = roipoly(M(1).cdata);
-ptr = find(P);
-
-M1 = double(M(1).cdata);
-R = M1(:,:,1);
-G = M1(:,:,2);
-B = M1(:,:,3);
+% P = roipoly(M(1).cdata);
+% ptr = find(P);
+% 
+% M1 = double(M(1).cdata);
+% R = M1(:,:,1);
+% G = M1(:,:,2);
+% B = M1(:,:,3);
