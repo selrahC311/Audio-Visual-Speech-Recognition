11fef9d3623fc0c227fcf843475f67b362b1528e
DylanN508 <98781315+DylanN508@users.noreply.github.com>
2022-12-12 01:19:42 +0000

    Update DCT.m

--- DCT.m
+++ DCT.m
@@ -3,33 +3,33 @@ vidHeight = v.Height;
 vidWidth = v.Width;
 frames = read(v,1);
 
-
-
-
 rgbImage = im2double(frames);
+I = im2gray(rgbImage);
 
-rChannel = rgbImage(:,:,1);
-
-gChannel = rgbImage(:,:,2);
-
-bChannel = rgbImage(:,:,3);
+% rChannel = I(:,:,1);
+% 
+% gChannel = I(:,:,2);
+% 
+% bChannel = I(:,:,3);
 a = zeros(size(frames, 1 ),size(frames, 2));
 
-Rdct = dct2(rChannel);
-Gdct = dct2(gChannel);
-Bdct = dct2(bChannel);
+% Rdct = dct2(rChannel);
+% Gdct = dct2(gChannel);
+% Bdct = dct2(bChannel);
+img_new = dct2(I);
+
+img_new1 = idct2(img_new);
 
 % just_red = cat(3,rChannel,a,a);
 
-img_new = idct2(Rdct); idct2(Gdct); idct2(Bdct);
+% img_new = idct2(Rdct); idct2(Gdct); idct2(Bdct);
 
 %dct of red plane
 % imshow(Rdct);
 
 %idct of full imagea
-imshow(img_new);
+imshow(img_new1);
 figure;
-imshow(log(abs(img_new)), [], 'colormap', jet(64))
 
 %original image
 % imshow(frames);
