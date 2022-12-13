6beef6017a9bbc7b0842dc6a88048cd9afa75191
selrahC311 <charles.yt.tsang@gmail.com>
2022-12-13 01:21:13 +0000

    remove unused files

--- LinearImageFilter.m
+++
@@ -1,7 +0,0 @@
-z = fspecial('filtertype', hsize, sigma);
-% provides a means for constructing filter kernels for many different
-% filter types
-
-x = imfilter(A, h, padding, outputSize, filterOperation);
-% then apply the kernel to an image, where the desired behaviour at the
-% image borders can be specified
